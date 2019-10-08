25ef35b66e41d4544f25dbcd7c55f3d71568ef45
Yunoinsky <wenchen9@qq.com>
2019-10-08 15:06:07 +0800

    texthandle

--- texthandle.lua
+++ texthandle.lua
@@ -13,21 +13,9 @@ function TIC()
 end
 
 
-
+-- the single-line inputbox, it will create and return a new inputBox object
+-- x, y the topleft point coordinate of the inp 
 function inputBox(x,y,length, fcolor, bcolor)
-   local framewidth = 2
-   local width = length*8+2*framewidth
-   local height = 8 + framewidth
-   local x1 = x
-   local x2 = x+width
-   local y1 = y
-   local y2 = y+height
-      
-   return {
-      onFocus = false,  --if the input box get the focus
-      t = 0,
-      handleInput = function()
-      end
-   }
+   
 end
 
