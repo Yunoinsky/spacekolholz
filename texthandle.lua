dc26c83416af8ea6c4f4884dc64253f0ff694ed1
Yunoinsky <wenchen9@qq.com>
2019-10-12 18:58:00 +0800

    haha

--- texthandle.lua
+++ texthandle.lua
@@ -12,6 +12,23 @@ function TIC()
    
 end
 
+keymap = {
+   [1] = 'a',
+   [2] = 'b',
+   [3] = 'c',
+   [4] = 'd',
+   [5] = 'e',
+   [6] = 'f',
+   [7] = 'g',
+   [8] = 'h',
+   [9] = 'i',
+   [10] = 'j',
+   [11] = 'k',
+   [12] = 'l',
+   
+
+
+
 
 
 function inputBox(x,y,length, fcolor, bcolor)
@@ -27,6 +44,7 @@ function inputBox(x,y,length, fcolor, bcolor)
       onFocus = false,
       t = 0,
       handleInput = function()
+	 
       end
    }
 end
