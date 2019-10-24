9502bd3ed0b1547cc7c6455170d0c88496bbe346
Yunoinsky <wenchen9@qq.com>
2019-10-24 10:25:07 +0800

    OK

--- texthandle.lua
+++ texthandle.lua
@@ -9,8 +9,8 @@ function TIC()
    cls()
    a = key(7)
    print(a,11,31, 1, false)
-   
 end
+
 function get_kbd_char()
    A="abcdefghijklmnopqrstuvwxyz0123456789-=[]\\;'`,./ "
    S="ABCDEFGHIJKLMNOPQRSTUVWXYZ)!@#$%^&*(_+{}|:\"~<>? "
@@ -18,7 +18,6 @@ function get_kbd_char()
       local c=peek(0xff88+i)
       if c>0 and c<=#A and keyp(c,20,3) then
 	 return key(64)and S:sub(c,c)or A:sub(c,c)
- 
      end
    end
    return nil
@@ -31,7 +30,7 @@ end
 --
 
 
-function inputBox(x_lt,y_lt,length, f_color, b_color)
+function inputBox(x_lt,y_lt,length, f_color, b_color, hintword)
    local ipb = {
       x = x_lt,
       y = y_lt,
@@ -39,10 +38,20 @@ function inputBox(x_lt,y_lt,length, f_color, b_color)
       fcolor = f_color,
       bcolor = b_color,
       onfocus = false,
-      buf = ""
+      buf = "",
+      hint = hintword or ""
    }
    function ipb:handleInput()
-       
+      if self.onfocus then
+	 c = get_kbd_char()
+	 if c then
+	    self.buf = self.buf .. c
+	 end
+      end
    end
+   function ipb:update()
+      
+   end
+   return ipb
 end
 
