2751c50ffa6e446b8b37e216ee25415de0553dd4
Yunoinsky <wenchen9@qq.com>
2019-11-12 20:19:12 +0800

    update yui

--- yui.lua
+++ yui.lua
@@ -10,13 +10,29 @@ end
 
 P.yui_meta = {
    __index = {
+      update = function(self, ...)
+	 self[1]:t_map_r(function(map_node_ptr, ...) map_node_ptr[0]:update(...) end, ...)
+      end,
+      draw = function(self)
+	 self[1]:t_map_r(function(map_node_ptr, ...) map_node_ptr[0]:draw(...) end, ...)
+      end,
+      
       
    }
 }
 
-P.init_yui = function()
-   local new_yui = {}
+P.frame = function()
    
+end
+
+P.yui = function()
+   local new_yui = {
+      [0] = "New_yui" -- yui name
+      [1] = tree.scene_tree()
+      [2] = "yui"
+   }
+   setmetatable(new_yui, P.yui_meta)
+   return new_yui
 end
 
 
